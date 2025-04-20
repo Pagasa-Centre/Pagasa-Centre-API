94a8f445f5c3a1c01c779b8383eaf43c5ec31ea0
Lionel Wilson <Lionel_Wilson@outlook.com>
2025-04-20 01:52:50 +0100

    make 2 user mappers files in service layer

--- internal/user/mappers/domain_to_entity.go
+++ internal/user/mappers/domain_to_entity.go
@@ -19,27 +19,3 @@ func ToUserEntity(user domain.User) *entity.User {
 		OutreachID:     null.StringFrom(user.OutreachID),
 	}
 }
-
-func UserEntityToUserDomain(user *entity.User) *domain.User {
-	var phoneNumber string
-	if user.Phone.Valid {
-		phoneNumber = user.Phone.String
-	}
-
-	var cellLeaderID *string
-	if user.CellLeaderID.Valid {
-		cellLeaderID = &user.CellLeaderID.String
-	}
-
-	return &domain.User{
-		ID:             user.ID,
-		FirstName:      user.FirstName,
-		LastName:       user.LastName,
-		Email:          user.Email,
-		HashedPassword: user.HashedPassword,
-		Birthday:       user.Birthday.Time,
-		PhoneNumber:    phoneNumber,
-		CellLeaderID:   cellLeaderID,
-		OutreachID:     user.OutreachID.String,
-	}
-}
